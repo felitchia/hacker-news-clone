8d7530ab8aceb924863f9505e96af7af986a3b66
felitchia <felitchia@gmail.com>
2017-05-09 17:14:30 +0100

    add logout view and fix layout user login check

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
-from flask_login import login_user , logout_user , current_user , login_required, LoginManager
+from flask_login import login_user, logout_user, current_user, login_required, LoginManager
 from flask_sqlalchemy import SQLAlchemy
 import os
 import models
@@ -32,12 +32,15 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
+
 @app.route('/')
 def index():
     articles = db.session.query(models.Article)
     return render_template('index.html', articles=articles)
 
+
 @app.route('/submit', methods=['GET', 'POST'])
+@login_required
 def submit_article():
     if request.method == 'GET':
         return render_template('submit.html')
@@ -47,6 +50,53 @@ def submit_article():
     flash('Your article was successfully inserted')
     return redirect(url_for('index'))
 
+
+@login_manager.user_loader
+def load_user(username):
+    try:
+        return models.User.query.get(username)
+    except:
+        return None
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'GET':
+        return render_template('signup.html')
+    username = request.form['username']
+    password = request.form['password']
+    email = request.form['email']
+    if models.User.query.filter_by(username=username).first() is not None:
+        flash('Username already in use')
+    elif models.User.query.filter_by(email=email).first() is not None:
+        flash('Email already registered. Would you like to recover your password?')
+    else:
+        usr = models.User(username, email, password)
+        db.session.add(usr)
+        db.session.commit()
+        flash('You\'ve been successfully registered')
+    return redirect(url_for('login'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    username = request.form['username']
+    password = request.form['password']
+    user = models.User.query.filter_by(username=username, password=password).first()
+    if user is None:
+        flash('Username or Password is invalid', 'error')
+        return redirect(url_for('login'))
+    login_user(user)
+    flash('You\'ve been successfully logged in')
+    return redirect(request.args.get('next') or url_for('index'))
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run()
--- create_db.py
+++ create_db.py
@@ -1,5 +1,5 @@
 from models import db
-from models import Article
+from models import Article, User
 
 # create the database and the db table
 db.create_all()
